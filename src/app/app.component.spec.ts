7e8686dc0288028e60a712af0999390b21c2f1ab
JosmiJoseph7 <85603252+JosmiJoseph7@users.noreply.github.com>
2023-03-31 23:20:09 +0530

    name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mean-demo2-client'`, () => {
+  it(`should have as title 'angularD3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mean-demo2-client');
+    expect(app.title).toEqual('angularD3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mean-demo2-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularD3 app is running!');
   });
 });
